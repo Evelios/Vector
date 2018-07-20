28f8e6a0b335321feedc9730bb8c1c411d2cb8bb
Thomas Waters <twaters117@gmail.com>
2018-07-20 08:07:21 -0400

    Reverted to ES5 Standards

--- Vector.js
+++ Vector.js
@@ -1,4 +1,4 @@
-class Vector {
+module.exports = (function() {
     /**
      * @class Vector
      *
@@ -13,6 +13,9 @@ class Vector {
      */
 
     //---- Default Constructor ----
+
+    const _p = 8;
+    const Precision = Math.pow(10, _p);
 
     /**
      * Create a vector object from a List or Object type vector notation.
@@ -34,22 +37,22 @@ class Vector {
      * @throws {TypeError} If the array is NaN or infinity
      * @memberof Vector
      */
-    static Vector(x, y) {
+    const Vector = function(x, y) {
         if (Array.isArray(x)) {
             if (x.length == 2) {
-                return [Vector._clean(x[0]), Vector._clean(x[1])];
+                return [_clean(x[0]), _clean(x[1])];
            }
            else {
                throw new ValueError('Vector is of length ' + x.length + ' instead of length 2');
            }
         }
         else if (x.hasOwnProperty('x') && x.hasOwnProperty('y')) {
-            return [Vector._clean(x.x), Vector._clean(x.y)];
+            return [_clean(x.x), _clean(x.y)];
         }
         else {
-            return [Vector._clean(x), Vector._clean(y)];
+            return [_clean(x), _clean(y)];
         }
-    }
+    };
 
     //---- Alternate Polar Constructor ----
 
@@ -65,9 +68,9 @@ class Vector {
      *
      * @memberof Vector
      */
-    static Polar(radius, theta) {
-        return Vector.Vector(radius * Math.cos(theta), radius * Math.sin(theta));
-    }
+    const Polar = function(radius, theta) {
+        return Vector(radius * Math.cos(theta), radius * Math.sin(theta));
+    };
 
     //---- Helper Functions ----
     
@@ -85,7 +88,7 @@ class Vector {
      * @throws {RangeError} Throws range error if the value is NaN or -Inf or +Inf 
      * @memberof Vector
      */
-    static _clean(num) {
+    const _clean = function(num) {
         if (isNaN(num)) {
             throw new RangeError('Value is NaN');
         }
@@ -98,12 +101,12 @@ class Vector {
             return num;
         }
 
-        return Math.round(num * Vector.Precision) / Vector.Precision;
-    }
+        return Math.round(num * Precision) / Precision;
+    };
 
     /**
      * Determine if two numbers are almost equal to eachother. This is based on
-     * the Vector.Precision value
+     * the Precision value
      * 
      * @static
      * @private
@@ -113,9 +116,9 @@ class Vector {
      * 
      * @memberof Vector
      */
-    static _almostEqual(a, b) {
-        return Vector._clean(a) - Vector._clean(b) < Vector.Precision;
-    }
+    const _almostEqual = function(a, b) {
+        return _clean(a) - _clean(b) < Precision;
+    };
 
     /**
      * Determine if two vectors are equal to eachother
@@ -127,10 +130,10 @@ class Vector {
      * 
      * @memberof Vector
      */
-    static equals(a, b) {
-        return Vector._almostEqual(a[0], b[0]) &&
-               Vector._almostEqual(a[0], b[1]);
-    }
+    const equals = function(a, b) {
+        return _almostEqual(a[0], a[1]) &&
+               _almostEqual(b[0], b[1]);
+    };
 
     /**
      * Returns the vector as a string of (x, y)
@@ -140,9 +143,9 @@ class Vector {
      * @returns {string} The string representation of a vector in (x, y) form
      * @memberof Vector
      */
-    static toString(vec) {
+    const toString = function(vec) {
         return `(${vec[0]}, ${vec[1]})`;
-    }
+    };
 
     /**
      * Get a copy of the input vector
@@ -152,9 +155,9 @@ class Vector {
      * @returns {Vector} The vector copy
      * @memberof Vector
      */
-    static copy(vec) {
-        return Vector.Vector(vec);
-    }
+    const copy = function(vec) {
+        return Vector(vec);
+    };
 
     //---- Vector Properties ----
 
@@ -165,9 +168,9 @@ class Vector {
      * @returns {number} The magniture of the vector
      * @memberof Vector
      */
-    static magnitude(vec) {
-        return Math.sqrt(Vector.magSquared(vec));
-    }
+    const magnitude = function(vec) {
+        return Math.sqrt(magSquared(vec));
+    };
 
     /**
      * Get the magnitude of the vector squared. Use this value if you only need
@@ -181,9 +184,9 @@ class Vector {
      * 
      * @memberof Vector
      */
-    static magSquared(vec) {
+    const magSquared = function(vec) {
         return Math.pow(vec[0], 2) + Math.pow(vec[1], 2);
-    }
+    };
 
     /**
      * Get the angle of the input vector
@@ -194,13 +197,13 @@ class Vector {
      * 
      * @memberof Vector
      */
-    static angle(vec) {
+    const angle = function(vec) {
         const x = vec[0];
         const y = vec[1];
         if (x === 0) {
             return y == 0 ?     0          :
                    y >  0 ?     Math.PI / 2:
-                            3 * Math.PI / 2; 
+                            3 * Math.PI / 2;
         } else if (y === 0) {
             return x >= 0 ? 0 : Math.PI;
         }
@@ -212,7 +215,7 @@ class Vector {
                                    angle             ; // Quadrant I
         
         return angle;
-    }
+    };
 
     //---- Basic Math Functions ----
 
@@ -225,9 +228,9 @@ class Vector {
      * @returns {Vector} The vector result of adding the two vectors
      * @memberof Vector
      */
-    static add(a, b) {
-        return Vector.Vector(a[0] + b[0], a[1] + b[1]);
-    }
+    const add = function(a, b) {
+        return Vector(a[0] + b[0], a[1] + b[1]);
+    };
 
     /**
      * Subtract two vectors element wise
@@ -238,9 +241,9 @@ class Vector {
      * @returns {Vector} The vector result of subtracting the two vectors
      * @memberof Vector
      */
-    static subtract(a, b) {
-        return Vector.Vector(a[0] - b[0], a[1] - b[1]);
-    }
+    const subtract = function(a, b) {
+        return Vector(a[0] - b[0], a[1] - b[1]);
+    };
 
     /**
      * Multiply the vector by a scalar value
@@ -251,9 +254,9 @@ class Vector {
      *  element wise
      * @memberof Vector
      */
-    static multiply(vec, scalar) {
-        return Vector.Vector(vec[0] * scalar, vec[1] * scalar);
-    }
+    const multiply = function(vec, scalar) {
+        return Vector(vec[0] * scalar, vec[1] * scalar);
+    };
 
     /**
      * Divide the vector by a scalar value
@@ -263,9 +266,9 @@ class Vector {
      * @returns {Vector} The result of multiplying the vector by a scalar
      * @memberof Vector
      */
-    static divide(vec, scalar) {
-        return Vector.Vector(vec[0] / scalar, vec[1] / scalar);
-    }
+    const divide = function(vec, scalar) {
+        return Vector(vec[0] / scalar, vec[1] / scalar);
+    };
 
     //---- Advanced Vector Functions ----
     
@@ -276,9 +279,9 @@ class Vector {
      * @returns {Vector} A vector that is the normal compenent of the vector
      * @memberof Vector
      */
-    static normalize(vec) {
-        return Vector.divide(vec, Vector.magnitude(vec));
-    }
+    const normalize = function(vec) {
+        return divide(vec, magnitude(vec));
+    };
 
     /**
      * Get the get the current vector rotated by a certain ammount clockwise
@@ -294,7 +297,7 @@ class Vector {
      *  vector by a particular ammount
      * @memberof Vector
      */
-    static rotate(vec, around, angle) {
+    const rotate = function(vec, around, angle) {
         const x = vec[0];
         const y = vec[1];
         const x_origin = around[0];
@@ -304,8 +307,8 @@ class Vector {
                           ((y_origin - y) * Math.sin(angle)) + x_origin;
         const y_rotated = ((y_origin - y) * Math.cos(angle)) -
                           ((x - x_origin) * Math.sin(angle)) + y_origin;
-        return Vector.Vector(x_rotated, y_rotated);
-    }
+        return Vector(x_rotated, y_rotated);
+    };
 
     /**
      * Get the dot product of two vectors
@@ -316,9 +319,9 @@ class Vector {
      * @returns {number} The dot product of the two vectors
      * @memberof Vector
      */
-    static dot(a, b) {
+    const dot = function(a, b) {
         return a[0] * b[0] + a[1] * b[1];
-    }
+    };
 
     /**
      * Get the average location between several vectors
@@ -326,14 +329,14 @@ class Vector {
      * @param {Vector[]} vectors The list of vectors to average
      * @memberof Vector
      */
-    static avg(vectors) {
-        let average = Vector.zero();
+    const avg = function(vectors) {
+        let average = zero();
 
         for (const vector of vectors) {
-            average = Vector.add(average, vector);
+            average = add(average, vector);
         }
-        return Vector.divide(average, vectors.length);
-    }
+        return divide(average, vectors.length);
+    };
 
     /**
      * Get the cross product of two vectors
@@ -344,9 +347,9 @@ class Vector {
      * @returns {number} The cross product of the two vectors
      * @memberof Vector
      */
-    static cross(a, b) {
+    const cross = function(a, b) {
         return a[0] * b[1] - a[1] * b[0];
-    }
+    };
 
     /**
      * Get the midpoint between two vectors
@@ -357,9 +360,9 @@ class Vector {
      * @returns The midpoint of two vectors
      * @memberof Vector
      */
-    static midpoint(a, b) {
-        return Vector.divide(Vector.add(a, b), 2);
-    }
+    const midpoint = function(a, b) {
+        return divide(add(a, b), 2);
+    };
 
     /**
      * Get the projection of vector a onto vector b
@@ -372,9 +375,9 @@ class Vector {
      *
      * @todo Add assertion for non-zero length b vector
      */
-    static proj(a, b) {
-        return Vector.multiply(b, Vector.dot(a, b) / Math.pow(Vector.magnitude(b), 2));
-    }
+    const proj = function(a, b) {
+        return multiply(b, dot(a, b) / Math.pow(magnitude(b), 2));
+    };
 
     /**
      * Get the angle between two vectors
@@ -385,9 +388,9 @@ class Vector {
      * @returns The angle between vector a and vector b
      * @memberof Vector
      */
-    static angleBetween(a, b) {
-        return Math.acos(Vector.dot(a, b) / (Vector.magnitude(a) * Vector.magnitude(b)));
-    }
+    const angleBetween = function(a, b) {
+        return Math.acos(dot(a, b) / (magnitude(a) * magnitude(b)));
+    };
 
     /**
      * Get the euclidean distance between two vectors
@@ -399,9 +402,9 @@ class Vector {
      * @see {@link distSquared}
      * @memberof Vector
      */
-    static distance(a, b) {
-        return Math.sqrt(Vector.distSquared(a, b));
-    }
+    const distance = function(a, b) {
+        return Math.sqrt(distSquared(a, b));
+    };
 
     /**
      * Get the euclidean distnace squared between two vectors.
@@ -415,11 +418,11 @@ class Vector {
      * @see {@link distnace}
      * @memberof Vector
      */
-    static distSquared(a, b) {
+    const distSquared = function(a, b) {
         const dx = a[0] - b[0];
         const dy = a[1] - b[1];
         return dx * dx + dy * dy;
-    }
+    };
 
     /**
      * Get the shortest distance between the point p and the line
@@ -434,9 +437,9 @@ class Vector {
      * @see {@link http://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment}
      * @memberof Vector
      */
-    static distToSeg(p, v, w) {
-        return Math.sqrt(Vector.distToSegSquared(p, v, w));
-    }
+    const distToSeg = function(p, v, w) {
+        return Math.sqrt(distToSegSquared(p, v, w));
+    };
 
     /**
      * Get the shortest distance squared between the point p and the line
@@ -451,18 +454,17 @@ class Vector {
      * @see {@link http://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment}
      * @memberof Vector
      */
-    static distToSegSquared(p, v, w) {
-        const l = Vector.distSquared(v, w);
+    const distToSegSquared = function(p, v, w) {
+        const l = distSquared(v, w);
         if (l === 0) {
-            return Vector.distSquared(p, v);
+            return distSquared(p, v);
         }
         let t = ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l;
         t = Math.max(0, Math.min(1, t));
-        return Vector.distSquared(
-            p,
-            Vector.Vector(v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1]))
+        return distSquared(
+            p, Vector(v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1]))
         );
-    }
+    };
 
     /**
      * Get the two normal vectors that are perpendicular to the current vector
@@ -474,11 +476,11 @@ class Vector {
      *  -PI/2 rad.
      * @memberof Vector
      */
-    perpendiculars(vec) {
-        const plus90 = Vector.Vector(-vec[1], vec[0]).normalize();
-        const minus90 = Vector.Vector(vec[1], -vec[0]).normalize();
+    perpendiculars = function(vec) {
+        const plus90 = Vector(-vec[1], vec[0]).normalize();
+        const minus90 = Vector(vec[1], -vec[0]).normalize();
         return [plus90, minus90];
-    }
+    };
 
     //---- Standard Static Vector Objects ----
 
@@ -490,10 +492,10 @@ class Vector {
      * @returns {Vector} Vector of magnitude zero
      * @memberof Vector
      */
-    static zero() {
+    const zero = function() {
         "use strict";
         return [0, 0];
-    }
+    };
 
     /**
      * Get the unit vector pointing in the positive y direction
@@ -503,10 +505,10 @@ class Vector {
      * @returns {Vector} Unit vector pointing up
      * @memberof Vector
      */
-    static up() {
+    const up = function() {
         "use strict";
         return [0, 1];
-    }
+    };
 
     /**
      * Get the unit vector pointing in the negative y direction
@@ -516,10 +518,10 @@ class Vector {
      * @returns {Vector} Unit vector pointing down
      * @memberof Vector
      */
-    static down() {
+    const down = function() {
         "use strict";
         return [0, -1];
-    }
+    };
 
     /**
      * Get the unit vector pointing in the negative x direction
@@ -529,10 +531,10 @@ class Vector {
      * @returns {Vector} Unit vector pointing right
      * @memberof Vector
      */
-    static left() {
+    const left = function() {
         "use strict";
         return [-1, 0];
-    }
+    };
 
     /**
      * Get the unit vector pointing in the positive x direction
@@ -542,14 +544,41 @@ class Vector {
      * @returns {Vector} Unit vector pointing right
      * @memberof Vector
      */
-    static right() {
+    const right = function() {
         "use strict";
         return [1, 0];
-    }
-}
+    };
 
-Vector._p = 8;
-Vector.Precision = Math.pow(10, Vector._p);
+    return {
+        Vector: Vector,
+        Polar: Polar,
+        equals: equals,
+        toString: toString,
+        copy: copy,
+        magnitude: magnitude,
+        magSquared: magSquared,
+        angle: angle,
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide,
+        normalize: normalize,
+        rotate: rotate,
+        dot: dot,
+        avg: avg,
+        cross: cross,
+        midpoint: midpoint,
+        proj: proj,
+        angleBetween: angleBetween,
+        distance: distance,
+        distSquared: distSquared,
+        distToSeg: distToSeg,
+        distToSegSquared: distToSegSquared,
+        zero: zero,
+        up: up,
+        down: down,
+        left: left,
+        right: right,
+    };
 
-module.exports = Vector;
-//export default Vector;
+}());
